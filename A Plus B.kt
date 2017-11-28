e9b9a2c54b74b2be6e72f3093405a6e63f658468
xiaolishang <faith.epiphone@qq.com>
2017-11-29 05:25:04 +0800

    iteration solution for a plus a

--- A Plus B.kt
+++ A Plus B.kt
@@ -21,10 +21,36 @@ fun plus(a: Int, b: Int): Int {
     return plus(sum, carry)
 }
 
+/**
+ * 迭代解法
+ */
+fun add(a: Int, b: Int): Int {
+    //不考虑进位
+    var sum = a xor b
+
+    //只考虑进位
+    var carry = (a and b) shl 1
+
+    while (carry != 0) {
+        val i = sum
+        val j = carry
+        sum = i xor j
+        carry = (i and j) shl 1
+    }
+
+    return sum
+}
+
 fun main(args: Array<String>) {
     println(plus(1, 5) == 1 + 5)
     println(plus(3, 6) == 3 + 6)
     println(plus(4, 4) == 4 + 4)
     println(plus(7, -1) == 7 + (-1))
     println(plus(-1, -1) == -1 + (-1))
+
+    println(add(1, 5) == 1 + 5)
+    println(add(3, 6) == 3 + 6)
+    println(add(4, 4) == 4 + 4)
+    println(add(7, -1) == 7 + (-1))
+    println(add(-1, -1) == -1 + (-1))
 }
